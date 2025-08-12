4fa859d6080a7dd4b372ae5dd149c26a80ccee21
Volodymyr Parakhonych <parakhonych@outlook.com>
2025-08-12 13:54:28 -0500

    Pallindrome task (2)

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        TwoSum Test = new TwoSum();
-        int[] nums1 = new int[]{2,7,11,15};
-        int target1 = 9;
-        System.out.println("Test 1: " + Arrays.toString(Test.twoSum(nums1,target1)));
 
-        int[] nums2 = new int[]{3,2,4};
-        int target2 = 6;
-        System.out.println("Test 2: " + Arrays.toString(Test.twoSum(nums2,target2)));
+        int target1 = 12441;
+        System.out.println("Test 2: " + String.valueOf(Palindrome.isPalindrome(target1)));
 
-        int[] nums3 = new int[]{2,5,5,15};
-        int target3 = 10;
-        System.out.println("Test 3: " + Arrays.toString(Test.twoSum(nums3,target3)));
+        int target2 = -121;
+        System.out.println("Test 2: " + String.valueOf(Palindrome.isPalindrome(target2)));
+
+        int target3 = 1221;
+        System.out.println("Test 2: " + String.valueOf(Palindrome.isPalindrome(target3)));
 
 
     }
---
+++ src/Palindrome.java
@@ -0,0 +1,58 @@
+/*
+* Given an integer x, return true if x is a
+
+, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+
+Constraints:
+
+    -231 <= x <= 231 - 1
+*/
+
+
+public  class Palindrome {
+    public static boolean isPalindrome(int x)
+    {
+        if (x<0){
+            return false;
+        }
+
+        String str = String.valueOf(x);
+
+        int left = 0;
+        int right = str.length()-1;
+
+        while (left<right)
+        {
+            if(str.charAt(left)!= str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+}
